a206ca20deb04e2b1c9c42cb0e3ba507ca08b1fb
CyberpunkLuna <60670362+CyberpunkLuna@users.noreply.github.com>
2025-09-01 17:07:09 -0600

    added asteroid logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     #init pygame and associated clock and screen objects
@@ -9,8 +11,18 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    #init the player object
+    #init groups for easy updating
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+
+    #init the game objects
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    asteroidfield = AsteroidField()
 
     #game loop
     while True:
@@ -18,8 +30,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
