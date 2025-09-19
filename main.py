d54f2ad4d8e6a995e503ae0ca5843b37a6094566
CyberpunkLuna <60670362+CyberpunkLuna@users.noreply.github.com>
2025-09-19 17:42:35 -0600

    displays text on respawn and death

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Meteoroids!')
+
+    #init font
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    respawn_text = font.render('You died... Respawning...', True, "white", "black")
+    respawn_rect = respawn_text.get_rect()
+    respawn_rect.center = (SCREEN_WIDTH/2), (SCREEN_HEIGHT/2)
+
+    gameover_text = font.render('GAME OVER', True, "white", "black")
+    gameover_rect = gameover_text.get_rect()
+    gameover_rect.center = (SCREEN_WIDTH/2), (SCREEN_HEIGHT/2)
 
     #init groups for easy updating
     updatable = pygame.sprite.Group()
@@ -39,13 +51,20 @@ def main():
         for asteroid in asteroids:
             if player.is_colliding(asteroid):
                 if player.death():
+                    screen.blit(gameover_text, gameover_rect)
+                    pygame.display.flip()
+                    pygame.time.delay(5000)
                     print("Game over!")
-                    score.update_time(pygame.time.get_ticks())
+                    score.update_time(pygame.time.get_ticks() - (RESPAWN_TIMER * 1000 * PLAYER_STARTING_LIVES))
                     score.print_score()
                     return
                 else:
                     for asteroid in asteroids:
                         asteroid.kill()
+                    screen.fill("black")
+                    screen.blit(respawn_text, respawn_rect)
+                    pygame.display.flip()
+                    pygame.time.delay(RESPAWN_TIMER * 1000)
                     player.respawn()
 
         for asteroid in asteroids:
